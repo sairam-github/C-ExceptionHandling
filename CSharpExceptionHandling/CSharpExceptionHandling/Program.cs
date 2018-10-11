8094642a95434f2aa151cc0638af581be2d2f086
Sairam Madem <sairam.madem@gmail.com>
2018-10-10 21:26:36 -0700

    ThrowVsThrowNew.

---
+++ CSharpExceptionHandling/CSharpExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+
+            int num2 = 50;
+
+            try
+            {
+                Divide(num1, num2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static int Divide(int num1, int num2)
+        {
+            try
+            {
+                return num2 / num1;
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw new Exception("Exception is thrown");
+                throw new Exception("Exception is thrown", ex);
+            }
+        }
+    }
+}
